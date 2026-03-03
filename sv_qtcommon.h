1bbf2d95118bc15bf6c1a0d99e8c0973fcd111a9
catch762 <hoh.prichesat@gmail.com>
2026-03-03 19:33:54 +0300

    init++

---
+++ sv_qtcommon.h
@@ -0,0 +1,15 @@
+//*******************************************************************************
+//
+// MODULE: sv_QtCommon
+//
+// DESCRIPTION: just common basic stuff for Qt that i use everywhere
+//
+// DEPENDENCIES:    - sv_Common
+//                  - QT
+//
+// USAGE: besides having dependencies met, you only need to include this header.
+//
+//*******************************************************************************
+
+#include "sv_common.h"
+#include "QtCommon.h"
