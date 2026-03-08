21ff2bf5bb8ca0f250e2e051dfe2478d1e259d0f
catch762 <hoh.prichesat@gmail.com>
2026-03-08 15:58:38 +0300

    wrappers

--- QtCommon.h
+++ QtCommon.h
@@ -24,3 +24,17 @@ inline QJsonArrayOpt jsonGetArrayOpt(const QJsonObject& json, const QString& key
     if (value.isArray()) return value.toArray();
     else return {};
 };
+
+//returns 0 if unregistered
+template <typename T>
+inline int qtTypeId()
+{
+    return QMetaType::fromType<T>().id();
+}
+
+//returns 0 if unregistered
+template <typename T>
+inline QString qtTypeName()
+{
+    return QMetaType::fromType<T>().name();
+}
