4e04a634a55a828c28e4f1822604f80f086246f0
catch762 <hoh.prichesat@gmail.com>
2026-06-21 18:28:01 +0300

    +

--- StdFormattersForQt.h
+++ StdFormattersForQt.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "sv_common.h"
 #include <QString>
+#include <QVariant>
+#include "QtCommon.h"
 
 SV_DECL_STD_FORMATTER(QString, obj.toStdString());
+SV_DECL_STD_FORMATTER(QVariant, qVariantInfo(obj));
 SV_DECL_STD_FORMATTER(std::optional<QString>, obj ? obj->toStdString() : std::string(""));
